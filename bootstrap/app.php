f394b0123e9ef50078a135b7c0a983d00f614ac9
tokyoshicity <67272832+tokyoshicity@users.noreply.github.com>
2024-05-24 12:24:26 +0500

    implemented basic auth and response with json middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,26 @@
 <?php
 
+use App\Http\Middleware\AuthenticateOnceWithBasicAuth;
+use App\Http\Middleware\ResponseWithJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->api([
+            ResponseWithJson::class
+        ]);
+
+        $middleware->alias([
+            'auth.basic.once' => AuthenticateOnceWithBasicAuth::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
