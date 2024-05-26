40ce5eebe2d1ad5457132a7051ea363ae6fdab1f
tokyoshicity <67272832+tokyoshicity@users.noreply.github.com>
2024-05-26 15:12:46 +0500

    added exceptions handling

--- app/Exceptions/ChatNotFoundException.php
+++ app/Exceptions/ChatNotFoundException.php
@@ -5,10 +5,10 @@ namespace App\Exceptions;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-class UserNotFoundException extends NotFoundHttpException
+class ChatNotFoundException extends NotFoundHttpException
 {
     public function __construct(
-        string $message = 'User not found',
+        string $message = 'Chat not found',
         int $code = Response::HTTP_NOT_FOUND
     ) {
         parent::__construct($message, code: $code);
